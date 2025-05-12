48eb0b42faa0db36214c20509d7b3b2a2d5437e5
lteixeiraufg <lucas.teixeira@discente.ufg.br>
2025-05-12 15:51:14 -0300

    Update Client.java

--- example/hello/Client.java
+++ example/hello/Client.java
@@ -35,6 +35,34 @@
  * intended for use in the design, construction, operation or
  * maintenance of any nuclear facility.
  */
+// package example.hello;
+
+// import java.rmi.registry.LocateRegistry;
+// import java.rmi.registry.Registry;
+
+// public class Client {
+
+//     private Client() {}
+
+//     public static void main(String[] args) {
+
+//         System.out.println("Initiating client");
+        
+//         String host = (args.length < 1) ? null : args[0];
+//         try {
+//             Registry registry = LocateRegistry.getRegistry(host);
+//             System.out.println("Registry has been located");
+//             Hello stub = (Hello) registry.lookup("Hello");
+//             System.out.println("Found server");
+//             String response = stub.sayHello();
+//             System.out.println("response: " + response);
+//         } catch (Exception e) {
+//             System.err.println("Client exception: " + e.toString());
+//             e.printStackTrace();
+//         }
+//     }
+// }
+
 package example.hello;
 
 import java.rmi.registry.LocateRegistry;
@@ -45,17 +73,16 @@ public class Client {
     private Client() {}
 
     public static void main(String[] args) {
-
         System.out.println("Initiating client");
-        
+
         String host = (args.length < 1) ? null : args[0];
         try {
             Registry registry = LocateRegistry.getRegistry(host);
-            System.out.println("Registry has been located");
             Hello stub = (Hello) registry.lookup("Hello");
-            System.out.println("Found server");
-            String response = stub.sayHello();
-            System.out.println("response: " + response);
+
+            String resposta = stub.eco("Isso é um teste RMI");
+            System.out.println("eco(): " + resposta);
+
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
